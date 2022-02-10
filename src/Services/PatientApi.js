d97430c4e65348d12c4aff40e334a57a2dc02d72
bv@bartosarchitecture.com <bv@bartosarchitecture.com>
2022-02-09 19:48:04 -0800

    Not props
    Just the value.

--- src/Services/PatientApi.js
+++ src/Services/PatientApi.js
@@ -1,10 +1,16 @@
 
 
-function getPatients(props){
+function getPatients(searchTerm){
 
-    console.log(props);
+    let url = 'https://6195803474c1bd00176c6d9a.mockapi.io/api/v1/patient';
 
-    return fetch('https://6195803474c1bd00176c6d9a.mockapi.io/api/v1/patient')
+    if  (searchTerm){
+
+        url =  url + '?search=' + searchTerm
+
+    }
+
+    return fetch(url)
             .then( res => res.json() )
             .then( (results) => {
                 console.log("getPatients api sucess retreiving patient data.");
@@ -13,6 +19,7 @@ function getPatients(props){
                 console.log("getPatients api error retriving patient data.");
                 console.log(error);
             })
+
 }
 
 export default getPatients;
