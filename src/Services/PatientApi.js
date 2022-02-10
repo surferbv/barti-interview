c7ae1574d73a292f7c8787afd3da83d1795728ca
bv@bartosarchitecture.com <bv@bartosarchitecture.com>
2022-02-09 16:03:10 -0800

    Refactor
    Changed all to just get.

--- src/Services/PatientApi.js
+++ src/Services/PatientApi.js
@@ -1,15 +1,18 @@
 
 
-function getAllPatients(){
+function getPatients(props){
+
+    console.log(props);
+
     return fetch('https://6195803474c1bd00176c6d9a.mockapi.io/api/v1/patient')
-            .then(res => res.json())
-            .then((results) => {
-                console.log("getAllPatients sucess retreiving patient data.");
+            .then( res => res.json() )
+            .then( (results) => {
+                console.log("getPatients api sucess retreiving patient data.");
                 return results
             }, (error) => {
-                console.log("getAllPatients error retriving patient data.");
+                console.log("getPatients api error retriving patient data.");
                 console.log(error);
             })
 }
 
-export default getAllPatients;
+export default getPatients;
