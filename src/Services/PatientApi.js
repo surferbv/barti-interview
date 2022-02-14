930e4e4548f6d1934458bd42907426e01b9f09df
bv@bartosarchitecture.com <bv@bartosarchitecture.com>
2022-02-14 15:00:44 -0800

    Comment out
    Not needed only used for trouble shooting.

--- src/Services/PatientApi.js
+++ src/Services/PatientApi.js
@@ -5,19 +5,21 @@ function getPatients(searchTerm){
     let url = 'https://6195803474c1bd00176c6d9a.mockapi.io/api/v1/patient';
 
     if  (searchTerm){
-
         url =  url + '?search=' + searchTerm
-
     }
 
     return fetch(url)
             .then( res => res.json() )
             .then( (results) => {
-                console.log("getPatients api sucess retreiving patient data.");
+
+                // console.log("getPatients api sucess retreiving patient data.");
                 return results
+
             }, (error) => {
+
                 console.log("getPatients api error retriving patient data.");
                 console.log(error);
+
             })
 
 }
