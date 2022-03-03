8f021c9b5be62dcaed6a3ed1e09795bccb41dd81
bv@bartosarchitecture.com <bv@bartosarchitecture.com>
2022-03-02 16:30:02 -0800

    Some Refactoring
    And some prep work for using server side pagination.

--- src/Components/PatientDataGrid.js
+++ src/Components/PatientDataGrid.js
@@ -1,36 +1,52 @@
 import * as React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
+import { useState } from 'react';
 
-const columns = [
-  { field: 'id', headerName: 'ID', width: 90 },
-  {
-    field: 'firstName',
-    headerName: 'First name',
-    width: 150,
-    editable: false,
-  },
-  {
-    field: 'lastName',
-    headerName: 'Last name',
-    width: 150,
-    editable: false,
-  }
-];
 
 export default function PatientDataGrid(props) {
-
+  
+  const titleColumns = [
+    { field: 'id', headerName: 'ID', width: 90 },
+    {
+      field: 'firstName',
+      headerName: 'First name',
+      width: 150,
+      editable: false,
+    },
+    {
+      field: 'lastName',
+      headerName: 'Last name',
+      width: 150,
+      editable: false,
+    }
+  ];
+  
   const patients = props.getPatients;
   
   const rows = patients;
 
+  const [rowState, setRowState] = useState({
+    page: 0,
+    pageSize: 5,
+    row:[],
+    loading: false
+  });
+
   return (
     <div style={{ height: 400, width: '100%' }}>
       <DataGrid
+        paginationMode='server'
+        rowCount={5} 
         className="center-block"
         rows={rows}
-        columns={columns}
+        columns={titleColumns}
         pageSize={5}
         rowsPerPageOptions={[5]}
+
+        onPageChange={ ( page )=>(
+          console.log("Page:", page)
+        )}
+
         disableSelectionOnClick
         sx={{ width:500 }}
       />
